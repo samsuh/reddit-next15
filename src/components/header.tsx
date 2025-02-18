475bbb61a7a463a09683a52673758c7ecf6541bc
Sam Suh <samsuh@gmail.com>
2025-02-19 03:44:30 +0900

    complete header and abstracted signedin status to client component to keep homepage static

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,59 +1,8 @@
 import Link from 'next/link'
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from '@heroui/react'
-import { auth } from '@/auth'
-import * as actions from '@/actions'
+import { Navbar, NavbarBrand, NavbarContent, Input } from '@heroui/react'
+import HeaderAuth from './header-auth'
 
 export default async function Header() {
-  const session = await auth()
-
-  let authContent: React.ReactNode
-  if (session?.user) {
-    authContent = (
-      <Popover placement='left'>
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ''} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className='p-4'>
-            <form action={actions.signOut}>
-              <Button type='submit'>Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    )
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type='submit' color='secondary' variant='bordered'>
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type='submit' color='primary' variant='flat'>
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    )
-  }
-
   return (
     <Navbar className='shadow mb-6'>
       <NavbarBrand>
@@ -64,7 +13,9 @@ export default async function Header() {
       <NavbarContent justify='center'>
         <Input />
       </NavbarContent>
-      <NavbarContent justify='end'>{authContent}</NavbarContent>
+      <NavbarContent justify='end'>
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   )
 }
