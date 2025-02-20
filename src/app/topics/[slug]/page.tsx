1c4e4af03b2640c587814c2d441858523dc6ff57
Sam Suh <samsuh@gmail.com>
2025-02-21 04:35:14 +0900

    build out topic show page, taking in params and displaying slug

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,17 @@
-export default function TopicShowPage() {
-  return <div>Topic Show Page</div>
+interface TopicShowPageProps {
+  params: Promise<{ slug: string }>
+}
+
+export default async function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = await params
+
+  return (
+    <div>
+      <div className='grid grid-cols-4 gap-4 p-4'>
+        <div className='col-span-3'>
+          <h1 className='text-2xl text-bold mb-2'>{slug}</h1>
+        </div>
+      </div>
+    </div>
+  )
 }
