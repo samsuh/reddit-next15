1151515a81c81fe73b9430ab603d50bd7c979a1b
Sam Suh <samsuh@gmail.com>
2025-02-23 05:53:06 +0900

    implement topicShowPage usage of post-list component which calls fetchData handed downn from parent component

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from '@/components/posts/post-create-form'
+import PostList from '@/components/posts/post-list'
+import { fetchPostByTopicSlug } from '@/db/queries/posts'
 
 interface TopicShowPageProps {
   params: Promise<{ slug: string }>
@@ -12,6 +14,7 @@ export default async function TopicShowPage({ params }: TopicShowPageProps) {
       <div className='grid grid-cols-4 gap-4 p-4'>
         <div className='col-span-3'>
           <h1 className='text-2xl text-bold mb-2'>{slug}</h1>
+          <PostList fetchData={() => fetchPostByTopicSlug(slug)} />
         </div>
         <PostCreateForm slug={slug} />
       </div>
---
+++ src/components/posts/post-list.tsx
@@ -0,0 +1,34 @@
+import type { PostWithData } from '@/db/queries/posts'
+import Link from 'next/link'
+import paths from '@/paths'
+
+interface PostListProps {
+  fetchData: () => Promise<PostWithData[]>
+}
+
+export default async function PostList({ fetchData }: PostListProps) {
+  const posts = fetchData()
+
+  const renderedPosts = (await posts).map((post) => {
+    const topicSlug = post.topic.slug
+    if (!topicSlug) {
+      throw new Error('Requires slug to link to post')
+    }
+
+    return (
+      <div key={post.id} className='border rounded p-2'>
+        <Link href={paths.postShow(topicSlug, post.id)}>
+          <h3 className='text-lg font-bold'>{post.title}</h3>
+          <div className='flex flex-row-gap-8'>
+            <p className='text-xs text-gray-400'>By {post.user.name}</p>
+            <p className='text-xs text-gray-400'>
+              {post._count.comments} comments
+            </p>
+          </div>
+        </Link>
+      </div>
+    )
+  })
+
+  return <div>{renderedPosts}</div>
+}
