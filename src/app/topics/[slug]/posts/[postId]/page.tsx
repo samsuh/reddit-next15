efebc0d2c535409035b5592b371cd14da76db45a
Sam Suh <samsuh@gmail.com>
2025-02-24 04:41:22 +0900

    create-comment-form implemented and nested comments should work.

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,9 @@
 import CommentCreateForm from '@/components/comments/comment-create-form'
 import PostShow from '@/components/posts/post-show'
+import CommentList from '@/components/comments/comment-list'
 import paths from '@/paths'
 import Link from 'next/link'
+import { fetchCommentsByPostId } from '@/db/queries/comments'
 
 interface PostShowPageProps {
   params: Promise<{ slug: string; postId: string }>
@@ -17,6 +19,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
         </Link>
         <PostShow postId={postId} />
         <CommentCreateForm postId={postId} startOpen />
+        <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
       </div>
     </div>
   )
